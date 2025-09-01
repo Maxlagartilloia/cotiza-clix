ddb77abac889f4587c61b300a31a8338cd0986f0
Copier Master <copiermastersd@gmail.com>
2025-09-01 13:20:22 +0000

    Como ingeniero de soporte senior de Firebase, con enfoque en Firestore,

---
+++ src/next.config.ts
@@ -0,0 +1,37 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+   allowedDevOrigins: [
+    'https://firebase-studio-1756698848467.cluster-f73iinkkulje66wssontdtbx6q.cloudworkstations.dev'
+  ],
+};
+
+export default nextConfig;
